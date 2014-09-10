ef7ea29c65009f5fa51130a8b7215ae85094a76b
brianhenning <brian.henning.m@gmail.com>
2014-09-10 08:32:07 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+var number = ('input');
+
+for(i = 0 ; i <= input; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if(i % 3 === 0) {
+        console.log("Fizz"); 
+    } else if(i % 5 === 0) {
+        console.log("Buzz");       
+    }else {
+        console.log(i);
+    }
+}
+
+});
